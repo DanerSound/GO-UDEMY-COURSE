e6805ef3559d1d01c9585c0c60f64241c1db10ee
danersound <andre.barreto28@gmail.com>
2023-09-22 15:14:21 +0200

    updated more about interface

--- 7-interfaceTest/main.go
+++ 7-interfaceTest/main.go
@@ -9,7 +9,7 @@ import (
 
 type logWriter struct{}
 
-func (logWriter) Write (bs []byte)(int, error){
+func (logWriter) Write (bs []byte)(int, error){  // custom implementation of write funtion that prints in the terminal
 	fmt.Println(string(bs))
 	fmt.Println("Just write all this Bytes", len(bs))
 	return len(bs),nil
@@ -25,9 +25,11 @@ func main() {
 	/* This is in general , but it can be semplify , se next session
 	bs := make([]byte, 99999) // empty holder that will contain the content that will arrive
 	resp.Body.Read(bs)
-	fmt.Println(string(bs))
-	*/
-	io.Copy(os.Stdout, resp.Body)
+	fmt.Println(string(bs)) 	*/
+
+	lw:= logWriter{}
+	io.Copy(lw, resp.Body) // my custom output
+	// io.Copy(os.Stdout, resp.Body) this output i can have it using 
 
 }
   
--- 9-Assig-Interface-Hard/main.go
+++ 9-Assig-Interface-Hard/main.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"os"
 )
 
+type fileWriter struct{}
+
+func (fileWriter) Read (bs []byte)(int, error){
+	fmt.Println(string(bs))
+	fmt.Println("Just write all this Bytes", len(bs))
+	return len(bs),nil
+}
+
+
+
 func main() {
 
-	fmt.Println(os.Args)
-	fmt.Println(os.Args[1])
+	fcont, err := os.OpenFile(os.Args[1], os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fw:= fileWriter{}
+
+	io.Copy(fw,fcont.)
+
 }
